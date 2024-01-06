7b541e29ebad2ccd64b21d51aa206e84fa35a64a
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-06 01:52:23 +0100

    better draggable

--- components/sidenav.vue
+++ components/sidenav.vue
@@ -1,61 +1,30 @@
 <script lang="ts" setup>
+import Draggable from 'vuedraggable';
+
 const props = defineProps(['slideshow', 'currentSlide'])
 const emit = defineEmits(['addSlide', 'set-current-slide'])
 
-
-let movingSlideIndex = null;
-let placeholderIndex = null;
-
-const handleDragStart = (index) => {
-  movingSlideIndex = index;
-  placeholderIndex = index;
-}
-
-const handleDragEnd = () => {
-  movingSlideIndex = null;
-  placeholderIndex = null;
-}
-
-const handleDragOver = (event) => {
-  const targetIndex = Number(event.dataTransfer.getData("text/plain"));
-  if (targetIndex !== placeholderIndex) {
-    placeholderIndex = targetIndex;
-  }
-}
-
-const handleDrop = (event) => {
-  event.preventDefault();
-  const targetIndex = Number(event.dataTransfer.getData("text/plain"));
-  if (targetIndex !== movingSlideIndex) {
-    const draggedItem = props.slideshow.slides.splice(movingSlideIndex, 1)[0];
-    props.slideshow.slides.splice(targetIndex, 0, draggedItem);
-  }
-  handleDragEnd();
-}
+const drag = ref(false);
 
 </script>
 
 <template>
   <div>
     <h5 class="text-h5">Übersicht</h5>
-    <div class="flex gap-2 flex-col my-2" @drop="handleDrop" @dragover.prevent>
-      <div class="w-full bg-neutral-400 aspect-video rounded overflow-hidden cursor-pointer border-2"
-           :class="{'border-blue-500': currentSlide === index, 'border-white': currentSlide !== index}"
-           @click="emit('set-current-slide', index)"
-           v-for="(slide,index) in slideshow.slides"
-           :key="slide.id"
-
-           draggable="true"
-           @dragend="handleDragEnd"
-           @dragstart="handleDragStart(index)"
-      >
-        <render-slide :slide="slide"
-                      style="width:1920px;height:1080px; transform: scale(0.12);transform-origin: top left; "/>
-        <div
-            v-if="placeholderIndex !== null"
-            class="slide-placeholder"
-        ></div>
-      </div>
+    <div class="flex gap-2 flex-col my-2">
+      <Draggable v-model="slideshow.slides" class="slides-container" @start="drag=true" @end="drag=false">
+        <template #item="{element: slide, index}"
+                  v-for="(slide,index) in slideshow.slides"
+                  :key="slide.id">
+          <div class="w-full bg-neutral-400 aspect-video rounded overflow-hidden cursor-pointer border-2"
+               :class="{'border-blue-500': currentSlide === index, 'border-white': currentSlide !== index}"
+               @click="emit('set-current-slide', index)"
+          >
+            <render-slide
+                style="width:1920px;height:1080px; transform: scale(0.12);transform-origin: top left; " :slide="slide"/>
+          </div>
+        </template>
+      </Draggable>
 
       <v-btn
           @click="emit('addSlide')"
