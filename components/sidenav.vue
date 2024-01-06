18f83defedbac04c206bb3b2c84d2cad137ddec5
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-06 01:47:17 +0100

    dragndrop folien sortiern

--- components/sidenav.vue
+++ components/sidenav.vue
@@ -1,25 +1,64 @@
 <script lang="ts" setup>
 const props = defineProps(['slideshow', 'currentSlide'])
 const emit = defineEmits(['addSlide', 'set-current-slide'])
+
+
+let movingSlideIndex = null;
+let placeholderIndex = null;
+
+const handleDragStart = (index) => {
+  movingSlideIndex = index;
+  placeholderIndex = index;
+}
+
+const handleDragEnd = () => {
+  movingSlideIndex = null;
+  placeholderIndex = null;
+}
+
+const handleDragOver = (event) => {
+  const targetIndex = Number(event.dataTransfer.getData("text/plain"));
+  if (targetIndex !== placeholderIndex) {
+    placeholderIndex = targetIndex;
+  }
+}
+
+const handleDrop = (event) => {
+  event.preventDefault();
+  const targetIndex = Number(event.dataTransfer.getData("text/plain"));
+  if (targetIndex !== movingSlideIndex) {
+    const draggedItem = props.slideshow.slides.splice(movingSlideIndex, 1)[0];
+    props.slideshow.slides.splice(targetIndex, 0, draggedItem);
+  }
+  handleDragEnd();
+}
+
 </script>
 
 <template>
   <div>
     <h5 class="text-h5">Übersicht</h5>
-    <div class="flex gap-2 flex-col my-2">
+    <div class="flex gap-2 flex-col my-2" @drop="handleDrop" @dragover.prevent>
       <div class="w-full bg-neutral-400 aspect-video rounded overflow-hidden cursor-pointer border-2"
            :class="{'border-blue-500': currentSlide === index, 'border-white': currentSlide !== index}"
            @click="emit('set-current-slide', index)"
            v-for="(slide,index) in slideshow.slides"
            :key="slide.id"
+
+           draggable="true"
+           @dragend="handleDragEnd"
+           @dragstart="handleDragStart(index)"
       >
-        <render-slide :slide="slide" style="width:1920px;height:1080px;
-        transform: scale(0.12);transform-origin: top left;
-"/>
+        <render-slide :slide="slide"
+                      style="width:1920px;height:1080px; transform: scale(0.12);transform-origin: top left; "/>
+        <div
+            v-if="placeholderIndex !== null"
+            class="slide-placeholder"
+        ></div>
       </div>
 
       <v-btn
-          @click="$emit('addSlide')"
+          @click="emit('addSlide')"
           variant="flat"
           color="#eee"
           prepend-icon="mdi-plus"
@@ -29,4 +68,13 @@ const emit = defineEmits(['addSlide', 'set-current-slide'])
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.slide-placeholder {
+  border: 2px dashed #ccc;
+  /* Weitere Stile für den Platzhalter */
+}
+
+.slide-preview {
+  transition: transform 0.3s ease;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-app>
-    <v-layout>
-      <v-container>
-        <slot/>
-      </v-container>
-    </v-layout>
-  </v-app>
+  <v-theme-provider theme="dark">
+    <v-app>
+      <v-layout>
+        <v-container>
+          <slot/>
+        </v-container>
+      </v-layout>
+    </v-app>
+  </v-theme-provider>
 </template>
